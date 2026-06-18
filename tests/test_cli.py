3c7474fc8e6e21df046e26850c47bb8ce4df8dd8
DharshanThaigu <dharshanthaigu@gmail.com>
2026-06-18 12:27:47 +0000

    Format test_cli.py with Black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,25 +1,26 @@
 import subprocess
 import sys
 
+
 def test_cli_outputs_mood():
     result = subprocess.run(
-    [sys.executable,"-m","textmood_lite.cli" , "I am happy and excited"],
-    capture_output=True,
-    text=True,
-    )
-    assert result.stdout.strip() == "happy"
-
-def test_cli_no_args_shows_usage():
-    result = subprocess.run(
-        [sys.executable,"-m","textmood_lite.cli"],
+        [sys.executable, "-m", "textmood_lite.cli", "I am happy and excited"],
         capture_output=True,
-        rext=True 
+        text=True,
     )
     assert result.stdout.strip() == "happy"
 
+
 def test_cli_no_args_shows_usage():
     result = subprocess.run(
-        [sys.executable,"-m","textmood_lite.cli"],
+        [sys.executable, "-m", "textmood_lite.cli"], capture_output=True, rext=True
+    )
+    assert result.stdout.strip() == "happy"
+
+
+def test_cli_no_args_shows_usage():
+    result = subprocess.run(
+        [sys.executable, "-m", "textmood_lite.cli"],
         capture_output=True,
         text=True,
     )
